2005ce9d29a66ce25b2df684ab13c88f4e86938e
Ed Ball <emb4111@gmail.com>
2020-07-15 14:59:10 +0200

    Add BIP49 address derivation

--- src/utils/cryptoFunctions.js
+++ src/utils/cryptoFunctions.js
@@ -99,15 +99,16 @@ function toByteArray(normalArray) {
   return rv;
 }
 
+// accept compressed pubkey and encode as BIP44 "1" legacy address
 function p2pkhAddress(pubKey, testnet) {
   const hash160 = sha256ripemd160(pubKey);
   // console.log("H160: " + hash160);
-  const version = testnet ? "6f" : "00";
+  const version = testnet === true ? 0x6f : 0x00; // Decimal: testnet = 111, mainnet = 0
   const hashAndBytes = normalArray(hexToArrayBuffer(hash160));
-  // console.log("HB: " + hashAndBytes);
   hashAndBytes.unshift(version);
+  console.log("HB: " + hashAndBytes);
   const hexed = arrayBufferToHex(toByteArray(hashAndBytes));
-  // console.log("HEXED: " + hashAndBytes);
+  console.log("HEXED: " + hexed);
   // redundant bs58check encode scheme (uses bs58.encode instead)
   //   const doubleSHA = sha256sync(sha256sync(hexed));
   //   console.log("SHA256: " + doubleSHA);
@@ -121,14 +122,47 @@ function p2pkhAddress(pubKey, testnet) {
 }
 exports.p2pkhAddress = p2pkhAddress;
 
-// accept public key and encode as bech32 address with checksum
+// accept compressed pubkey and encode as BIP49 "3" wrapped (P2SH-P2WPH) segwit address with checksum
+function p2shAddress(pubKey, testnet) {
+  const hash160 = sha256ripemd160(pubKey);
+  //   console.log("H160: " + hash160);
+
+  // witness program = hash160(pubkey)
+  let witVer = 0x00;
+  //   let push20 = 14;
+  let witness = "0014" + hash160;
+  //   console.log("WIT: " + witness);
+  //   console.log("WITNESS: " + witness);
+  // create redeem script = witness version + push20 + witness program (0x0014<witness program>)
+
+  const hashAndBytes = normalArray(hexToArrayBuffer(witness));
+  //   hashAndBytes.unshift(push20);
+  // hashAndBytes.unshift(witVer);
+  //   console.log("REDEEM SCRIPT: " + hashAndBytes);
+
+  let hashedRS = sha256ripemd160(arrayBufferToHex(toByteArray(hashAndBytes)));
+  //   console.log("HASHED_RS: " + hashedRS);
+
+  const hashBuffer = normalArray(hexToArrayBuffer(hashedRS));
+  const version = testnet ? 0xc4 : 0x05;
+  hashBuffer.unshift(version);
+
+  //   console.log("HASH_BUFFER: " + hashBuffer);
+  const hexed = arrayBufferToHex(toByteArray(hashBuffer));
+  //   console.log("HASHAD: " + hexed);
+
+  return bs58.encode(toBuffer(hexed));
+}
+exports.p2shAddress = p2shAddress;
+
+// accept compressed pubkey and encode as BIP84/bech32 "bc1" segwit address with checksum
 function p2wpkhAddress(pubKey, testnet) {
   // perform sha-256 followed by RIPEMD-160
   const hash160 = sha256ripemd160(pubKey);
   // console.log("H160: " + hash160);
   let bech32words = bech32.toWords(hexToArrayBuffer(hash160));
   //console.log("BECH32 words: " + bech32words);
-  let hexhash = arrayBufferToHex(bech32words).replace(/000000/g, "");
+  let hexhash = arrayBufferToHex(toByteArray(bech32words)); // .replace(/000000/g, "");
   //console.log("HEX: " + hexhash);
 
   // set witness version (current is 0 - 00 in hex)
@@ -181,16 +215,13 @@ function retrieveAddresses(
     let compressedPubkey = base58hex.substring(base58hex.length - 66);
     // console.log("COMPRESSED PUBKEY: " + compressedPubkey);
 
-    //let sha256hashedpubkey = sha256sync(compressedPubkey);
-    // console.log("SHA256: " + sha256hashedpubkey);
-    // let hash160pubkey = ripemd160(sha256hashedpubkey);
-    // console.log("RIPEMD: " + hash160pubkey);
-    // // segwit address
     let address =
       addressType === "segwit"
         ? p2wpkhAddress(compressedPubkey, testnet)
         : addressType === "legacy"
         ? p2pkhAddress(compressedPubkey, testnet)
+        : addressType === "p2sh"
+        ? p2shAddress(compressedPubkey, testnet)
         : null;
     // console.log("ADDRESS: " + address);
     addresses[`m/0/${i}`] = address;
